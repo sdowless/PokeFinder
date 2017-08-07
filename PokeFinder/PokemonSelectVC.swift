5a193428a2254b57f863dbcd101cd40d6e149fd1
Stephan Dowless <slcd09@yahoo.com>
2017-08-07 05:18:15 -0400

    Update PokemonSelectVC.swift

--- PokeFinder/PokemonSelectVC.swift
+++ PokeFinder/PokemonSelectVC.swift
@@ -7,29 +7,77 @@
 //
 
 import UIKit
+import MapKit
 
-class PokemonSelectVC: UIViewController {
-
+class PokemonSelectVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var pokeLocation: CLLocation!
+    var sendPoke: SendPokemon!
+    var pokeArray = [Pokemon]()
+    var poke: Pokemon!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        configureArray()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokeCell", for: indexPath) as? PokeCell
+        configureCell(cell: cell!, indexPath: indexPath)
+        
+        return cell!
     }
-    */
-
+    
+    func configureCell(cell: PokeCell, indexPath: IndexPath) {
+        
+        let poke = pokeArray[indexPath.row]
+        cell.configureCell(poke: poke)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        
+        let selectedPokemon = indexPath.row
+        print("Selected Pokemon: \(selectedPokemon)")
+        print("Pokemon Location: \(pokeLocation)")
+        
+        sendPoke = SendPokemon(location: pokeLocation, pokemonID: selectedPokemon)
+        
+        performSegue(withIdentifier: "MainVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MainVC"  {
+            let destination = segue.destination as! ViewController
+            destination.pokeLocation = sendPoke.location
+            destination.selectedPokemon = sendPoke.pokemonID
+            
+            print("Selected Pokemon: \(sendPoke.pokemonID)")
+            print("Pokemon Location: \(sendPoke.location)")
+        }
+    }
+    
+    func configureArray() {
+        
+        for i in 0..<pokemon.count {
+            let pokeName = pokemon[i]
+            let pokeID = i + 1
+            
+            let poke = Pokemon(name: pokeName, pokeID: pokeID)
+            pokeArray.append(poke)
+        }
+        
+    }
 }
+
+
